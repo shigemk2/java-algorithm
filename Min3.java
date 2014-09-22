682c02b2f46a2dafdd5ed04a17415d9be348bc53
shigemk2 <i.am.shige@gmail.com>
2014-09-23 00:59:24 +0900

    Exercise 1-2

---
+++ Min3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Min3 {
+    static int min3(int a, int b, int c) {
+        int min = a;
+
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("min3(1,2,3) = " + min3(1,2,3));
+        System.out.println("min3(1,3,2) = " + min3(1,3,2));
+        System.out.println("min3(3,1,2) = " + min3(3,1,2));
+        System.out.println("min3(3,3,0) = " + min3(3,3,0));
+        System.out.println("min3(0,0,0) = " + min3(0,0,0));
+    }
+}
